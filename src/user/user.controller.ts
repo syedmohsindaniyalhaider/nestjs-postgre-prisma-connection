7fe96af38467b751e15c0f6ad25f6797c4f8519b
Syed Mohsin <syedmohsindaniyalhaider@gmail.com>
2022-09-28 14:21:34 +0500

    #comment apis updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,26 +11,38 @@ import { RegisterUserDto } from './dto/register-user.dto';
 import { UserService } from './user.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { UsersDto } from './dto/users.dto';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
-  @Get()
-  findAll() {
-    this.userService.findAll();
+  @ApiResponse({
+    status: 200,
+    description: 'All Users',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Error',
+  })
+  @Get('/')
+  findAll(@Body() usersDto: UsersDto) {
+    return this.userService.findAll();
   }
   @ApiResponse({
     status: 201,
     description: 'User Successfully Registered',
   })
+  @ApiResponse({
+    status: 400,
+    description: 'Error',
+  })
   @Post('/register')
-  @HttpCode(201)
+  // @HttpCode(201)
   register(@Res() res: Response, @Body() registerUserDto: RegisterUserDto) {
     this.userService.register(registerUserDto);
     res
       .status(HttpStatus.CREATED)
       .send({ message: 'User Successfully Registered' });
-    // return response.status(201).send('User Successfully Registered');
   }
 }
